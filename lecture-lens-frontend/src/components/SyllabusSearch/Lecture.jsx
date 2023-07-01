6ef3431cc1a33e305eda56e9d9be6c673bffb35e
hirafish <95068237+hirafish@users.noreply.github.com>
2023-07-01 15:56:42 +0900

    授業表示をカード形式にしました

--- lecture-lens-frontend/src/components/SyllabusSearch/Lecture.jsx
+++ lecture-lens-frontend/src/components/SyllabusSearch/Lecture.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 export const Lecture = (props) => {
   const { details } = props;
@@ -6,16 +6,33 @@ export const Lecture = (props) => {
   const { searchText } = props;
   // レンダリングするかどうか
   const flag_array = Object.keys(filterClass).filter((output, index) => {
-    return filterClass[output] && details.class.includes(output)
+    return filterClass[output] && details.class.includes(output);
   });
   const count_array = Object.values(filterClass).filter((output, index) => {
-    return output === true
+    return output === true;
   });
   const searchFlag = details.name.includes(searchText);
 
   return (
     <>
-      { searchFlag && count_array.length === flag_array.length && <tr><td>{details.name} <a className={details.class} href={details.url} target="_blank" rel='noreferrer'><i className="far fa-file-alt icon"></i></a></td><td>{details.year_term}</td><td>{details.professor_name}</td><td>{details.key}</td></tr>}
+      {searchFlag && count_array.length === flag_array.length && (
+        <tr>
+          <td>
+            {details.name}{" "}
+            <a
+              className={details.class}
+              href={details.url}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="far fa-file-alt icon"></i>
+            </a>
+          </td>
+          <td>{details.year_term}</td>
+          <td>{details.professor_name}</td>
+          <td>{details.key}</td>
+        </tr>
+      )}
     </>
   );
 };
